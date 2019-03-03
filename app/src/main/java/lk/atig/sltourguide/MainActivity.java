648da1706ee1f1282d7c1c2698243a3b202185dc
Chandima Ranaweera <chan4lk@gmail.com>
2019-03-04 01:35:54 +0530

    modify single item view

--- app/src/main/java/lk/atig/sltourguide/MainActivity.java
+++ app/src/main/java/lk/atig/sltourguide/MainActivity.java
@@ -1,10 +1,14 @@
 package lk.atig.sltourguide;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +25,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         db = new DatabaseHelper(this);
 
         tourList = new ArrayList<>();
--- app/src/main/java/lk/atig/sltourguide/ViewTour.java
+++ app/src/main/java/lk/atig/sltourguide/ViewTour.java
@@ -15,15 +15,6 @@ public class ViewTour extends AppCompatActivity {
         setContentView(R.layout.activity_view_tour);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
