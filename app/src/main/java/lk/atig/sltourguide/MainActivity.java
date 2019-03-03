a87d5b88a8d3e390c8e46cade75bc7cb4ccac889
Chandima Ranaweera <chan4lk@gmail.com>
2019-03-03 23:59:24 +0530

    Initialize array

--- app/src/main/java/lk/atig/sltourguide/MainActivity.java
+++ app/src/main/java/lk/atig/sltourguide/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        toursList = new ArrayList<>();
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
